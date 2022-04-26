63af96c8bcd44bae10a436712060435cb74e48c1
saurabh2160 <saurabhmanohar90@gmail.com>
2022-04-26 17:06:03 +0530

    yes

--- model/model.js
+++ model/model.js
@@ -1,59 +1,62 @@
 const mongoose = require('mongoose');
 const ObjectId = mongoose.Schema.Types.ObjectId;
 
-const authorSchema = new mongoose.Schema( {
-    fname: { 
-       type:String,
-       required:true
-   }, 
-   lname: {
-       type:String,
-       required:true}, 
-   title: {
-       required:true,
-       enum:["Mr", "Mrs", "Miss"]
-   }, 
-   email: {
-       type:String,
-       unique:true}, 
-   password: {
-       type:String,
-       required:true
-   } 
-   
+const authorSchema = new mongoose.Schema({
+    fname: {
+        type: String,
+        required: true
+    },
+    lname: {
+        type: String,
+        required: true
+    },
+    title: {
+        required: true,
+        enum: ["Mr", "Mrs", "Miss"]
+    },
+    email: {
+        type: String,
+        unique: true
+    },
+    password: {
+        type: String,
+        required: true
+    }
+
 
 }, { timestamps: true });
 
 
 const blogSchema = new mongoose.Schema({
-    title: { 
-        typr:String,
-        required:true
-    }, 
+    title: {
+        typr: String,
+        required: true
+    },
     body: {
-        type:String,
-        required:true}, 
-    authorId: { 
-        type:ObjectId,
-        ref:"author"
-    }, 
-    tags: [String], 
-    category: { 
-        type:String,
-        required:true 
-    }, 
-   
-    subcategory:[String],  
+        type: String,
+        required: true
+    },
+    authorId: {
+        type: ObjectId,
+        ref: "author"
+    },
+    tags: [String],
+    category: {
+        type: String,
+        required: true
+    },
+
+    subcategory: [String],
     isDeleted: {
         type: Boolean,
-         default: false 
-        },
-         
+        default: false
+    },
+
     isPublished: {
-        type:Boolean, 
-        default: false 
-    } 
-  
+        type: Boolean,
+        default: false
+    }
+
 }, { timestamps: true })
 
 module.exports.blogSchema = mongoose.model('blog', blogSchema)
