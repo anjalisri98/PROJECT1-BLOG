7bde97377f33f2bfd62586b23e7c387ad799a64c
saurabh2160 <saurabhmanohar90@gmail.com>
2022-04-26 17:45:08 +0530

    yes

--- codecontroller/codecontroller.js
+++ codecontroller/codecontroller.js
@@ -1,1 +1,15 @@
-const {authorSchema,blogSchema}=require('../model/model')
+const authorSchema=require('../model/authormodel')
+const blogSchema=require('../model/blogmodel')
+
+let authordata=async (req,res)=>{
+    let data=req.body
+    let result=await authorSchema.create(data)
+    res.status(200).send({result})
+}
+let blogdata=async (req,res)=>{
+    let data=req.body
+    let result=await blogSchema.create(data)
+    res.status(200).send({result})
+}
+module.exports.authordata=authordata;
+module.exports.blogdata=blogdata;
--- route/route.js
+++ route/route.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const codecontrol=require('../codecontroller/codecontroller')
 const middleware=require('../middleware/middleware')
-
 const router = express.Router();
+
+router.post('/createauthor',codecontrol.authordata)
+router.post('/createblog',codecontrol.blogdata)
+
 module.exports = router;
