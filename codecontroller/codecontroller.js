f34982d0bf547a947a4cdd1f6f2c0c389a447fd2
saurabh2160 <saurabhmanohar90@gmail.com>
2022-04-28 20:25:34 +0530

    minor changes but good for summary and video

--- codecontroller/codecontroller.js
+++ codecontroller/codecontroller.js
@@ -56,20 +56,23 @@ let getBlog = async function (req, res) {
         isDeleted: false,
         isPublished: true,
       };
-      if (query.tags) {
-        query.tags = { $in: query.tags.split(",") };
+      if(Object.keys(query).length>0){
+        if (query.tags) {
+            query.tags = { $in: query.tags.split(",") };
+          }
+          if (query.subcategory) {
+            query.subcategory = { $in: query.subcategory.split(",") };
+          }
+          filter['$or'] = [
+            { authorId: query.authorId },
+            { category: query.category },
+            { subcategory: query.subcategory },
+            { tags: query.tags },
+            { body: query.body },
+            { title: query.title },
+          ];
       }
-      if (query.subcategory) {
-        query.subcategory = { $in: query.subcategory.split(",") };
-      }
-      filter['$or'] = [
-        { authorId: query.authorId },
-        { category: query.category },
-        { subcategory: query.subcategory },
-        { tags: query.tags },
-        { body: query.body },
-        { title: query.title },
-      ];
+    
       let filterByquery = await blogSchema.find(filter)
       if (filterByquery.length<=0) {
         return res.status(404).send({ msg: "Blog Not Found" });
