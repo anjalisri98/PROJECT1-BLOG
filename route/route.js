2225321e70600921ed184f6ae030d83ebc4f7ebf
saurabh2160 <saurabhmanohar90@gmail.com>
2022-05-02 16:07:45 +0530

    project final

--- route/route.js
+++ route/route.js
@@ -3,13 +3,13 @@ const codecontrol=require('../codecontroller/codecontroller')
 const middleware=require('../middleware/middleware')
 const router = express.Router();
 
-router.post('/createauthor',codecontrol.authordata)
-router.post('/authorlogin',codecontrol.loginauthor)
-router.post('/createblog',middleware.authentication,codecontrol.blogdata)
-router.get('/getblog',middleware.authentication,codecontrol.getBlog)
-router.put('/blogupdate/:blogId',middleware.autherization,codecontrol.blogUpdate)
-router.delete('/deleteblog/:blogId',middleware.autherization,codecontrol.delblog)
-router.delete('/deletebyquery',middleware.autherization,codecontrol.delbyquery)
+router.post('/authors',codecontrol.authordata)
+router.post('/login',codecontrol.loginauthor)
+router.post('/blogs',middleware.authentication,codecontrol.blogdata)
+router.get('/blogs',middleware.authentication,codecontrol.getBlog)
+router.put('/blogs/:blogId',middleware.autherization,codecontrol.blogUpdate)
+router.delete('/blogs/:blogId',middleware.autherization,codecontrol.delblog)
+router.delete('/blogs',middleware.autherization,codecontrol.delbyquery)
 
 
 module.exports = router;
